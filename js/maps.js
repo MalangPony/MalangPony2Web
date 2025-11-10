7a0a40d39dbb39b2a25ab4d3a5a374d1540ff5e2
SheepPony <sheepponymlp@gmail.com>
2025-11-10 18:39:42 +0900

    Improve map page

--- js/maps.js
+++ js/maps.js
@@ -38,7 +38,54 @@ var customOverlay = new kakao.maps.CustomOverlay({
     map: kkm,
     position: positionATM,
     content: overlayContent,
+    xAnchor:0.5,
     yAnchor: 0.0
+});
+
+let route_munrae=[
+  new kakao.maps.LatLng(37.51900312549791, 126.89473099985379), // Exit 3
+  new kakao.maps.LatLng(37.519041535116756, 126.89470100700552), //Road start
+  new kakao.maps.LatLng(37.52022853982451, 126.88718840508649), //Turn 1
+  new kakao.maps.LatLng(37.520765908380696, 126.88732130114812), //Turn 2
+  new kakao.maps.LatLng(37.52070183791423, 126.88768351702015) //Entrance
+];
+let line_munrae=new kakao.maps.Polyline({
+  map:kkm,
+  path:route_munrae,
+  strokeWeight:3,
+  strokeColor:"#FF0000",
+  strokeOpacity:0.7,
+  strokeStyle:"dashed"
+});
+
+let route_yangpyong=[
+  new kakao.maps.LatLng(37.525337054886194, 126.88603390409557), // Exit 2
+  new kakao.maps.LatLng(37.525230806644785, 126.88614554477905), //Road start
+  new kakao.maps.LatLng(37.52171598001462, 126.88529751693837), //Turn 1
+  new kakao.maps.LatLng(37.5213748337489, 126.88748158786863), //Turn 2
+  new kakao.maps.LatLng(37.520756986466395, 126.88732387473068), //Turn 3
+  new kakao.maps.LatLng(37.52070183791423, 126.88768351702015) //Entrance
+];
+
+
+let line_yangpyong=new kakao.maps.Polyline({
+  map:kkm,
+  path:route_yangpyong,
+  strokeWeight:3,
+  strokeColor:"#0000FF",
+  strokeOpacity:0.7,
+  strokeStyle:"dashed"
+});
+
+
+
+let drawing1_html='<img src="sprites-prototype/MPN2-Prototype-Image_MapDrawing_Munrae1.png" style="width:150px;height:150px;">'
+var drawing1 = new kakao.maps.CustomOverlay({
+    map: kkm,
+    position: new kakao.maps.LatLng(37.51852288426295, 126.89547104210291),
+    content: drawing1_html,
+    xAnchor:0.2,
+    yAnchor: 0.8
 });
 
 // Display button if venue is too off to the side
@@ -56,6 +103,13 @@ kakao.maps.event.addListener(kkm, 'bounds_changed', ()=>{
   if (centeredX && centeredY) mbj.style.display="none";
   else mbj.style.display="flex";
 });
+kakao.maps.event.addListener(kkm, 'zoom_changed', function() {
+  let zl=kkm.getLevel();
+    //console.log("ZL",zl);
+    if (zl>5.5) drawing1.setVisible(false);
+    else drawing1.setVisible(true);
+});
+
 mbj.style.display="none";
 mbj.addEventListener("click",()=>{
   kkm.panTo(positionATM);
