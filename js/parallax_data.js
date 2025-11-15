d00a563e7f55ef893fbe95c49ee35eb003292c86
SheepPony <sheepponymlp@gmail.com>
2025-11-15 16:21:14 +0900

    Add inquiries page

--- js/parallax_data.js
+++ js/parallax_data.js
@@ -167,6 +167,7 @@ export let camera_locations={
 	mascot:    [-200, camera_y,  -100, 1.0, camera_default_tilt],
 	credits:   [   0, camera_y,  1000, 1.0, camera_default_tilt],
 	conbook:   [-800, camera_y,   300, 1.0, camera_default_tilt],
+	inquiries: [-300, camera_y,   300, 1.0, camera_default_tilt]
 }
 
 // Camera parameters in sky mode.
