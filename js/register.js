dd53b86b604ad1aa8504ff922843bb50c4342e9c
SheepPony <sheepponymlp@gmail.com>
2025-11-21 00:10:48 +0900

    Change tier listing code and improve styling

--- js/register.js
+++ js/register.js
@@ -8,97 +8,110 @@ const tier_list=document.getElementById("register-tiers-list");
 
 
 for (const tier_id in TierData.tiers_data){
-	let tier=TierData.tiers_data[tier_id];
+	let tier_data=TierData.tiers_data[tier_id];
 	let tier_dom = tier_entry_template.content.cloneNode(true);
 	let tier_dom_inner=tier_dom.querySelector(".register-tier-entry");
 	
-	tier_dom_inner.classList.add(tier.css_class);
+	tier_dom_inner.classList.add(tier_data.css_class);
 	
-	tier_dom.querySelector(".register-tier-name.langdiv-ko").innerHTML=tier.name_ko;
-	tier_dom.querySelector(".register-tier-name.langdiv-en").innerHTML=tier.name_en;
+	tier_dom.querySelector(".register-tier-name.langdiv-ko").innerHTML=tier_data.name_ko;
+	tier_dom.querySelector(".register-tier-name.langdiv-en").innerHTML=tier_data.name_en;
 	
-	tier_dom.querySelector(".register-tier-description.langdiv-ko").innerHTML=tier.desc_ko;
-	tier_dom.querySelector(".register-tier-description.langdiv-en").innerHTML=tier.desc_en;
+	tier_dom.querySelector(".register-tier-description.langdiv-ko").innerHTML=tier_data.desc_ko;
+	tier_dom.querySelector(".register-tier-description.langdiv-en").innerHTML=tier_data.desc_en;
 	
 	tier_dom.querySelector(".register-tier-price.langdiv-ko").innerHTML=
-		tier.price+",000원";
+		tier_data.price+",000원";
 	tier_dom.querySelector(".register-tier-price.langdiv-en").innerHTML=
-		tier.price+",000 KRW";
+		tier_data.price+",000 KRW";
 	
-	if (Number.isFinite(tier.limit)){
+	if (Number.isFinite(tier_data.limit)){
 		tier_dom.querySelector(".register-tier-limit.langdiv-ko").innerHTML=
-			tier.limit+"명 한정";
+			tier_data.limit+"명 한정";
 		tier_dom.querySelector(".register-tier-limit.langdiv-en").innerHTML=
-			"limited to "+tier.limit+" persons";
+			"limited to "+tier_data.limit+" persons";
 	}else{
 		tier_dom.querySelector(".register-tier-limit.langdiv-ko").style.disply="none";
 		tier_dom.querySelector(".register-tier-limit.langdiv-en").style.disply="none";
 	}
 	
-	let tier_margin_top = tier.margin_top;
+	let tier_margin_top = tier_data.margin_top;
 	if (tier_margin_top) 
 		tier_dom.querySelector(".register-tier-entry").style.marginTop=tier_margin_top+"px";
 	
 	
-	let perks_id_list=[];
-	let perks_original_tier={};
-	let perk_overwritten_by={};
+	
+	// OOP programmer try not to use inheritance relationship in their code challenge (IMPOSSIBLE)
+	let tier_inheritance_chain=[]; //[0] is the root. [-1] is the current tier.
+	let perk_overwrite_map={}; // k gets overwritten by v
 	let tier_ptr=tier_id;
 	while (tier_ptr){
-		// Concatenate the perks list
-		perks_id_list=TierData.tiers_data[tier_ptr].perks_list.concat(perks_id_list);
-		// Set the original-tier map
-		for (const perk_id of TierData.tiers_data[tier_ptr].perks_list){
-			perks_original_tier[perk_id]=tier_ptr;
+		if (TierData.tiers_data[tier_ptr].perks_list.length>0){
+			tier_inheritance_chain.unshift(tier_ptr);
+			for( const perk_id of TierData.tiers_data[tier_ptr].perks_list ){
+				if (TierData.perks_data[perk_id].overwrites){
+					perk_overwrite_map[TierData.perks_data[perk_id].overwrites]=perk_id;
+				}
+			}
 		}
 		// Walk up the inheritance chain
 		tier_ptr=TierData.tiers_data[tier_ptr].inherits;
 	}
 	
-	// Check if overwritten
-	for( const perk_id of perks_id_list ){
-		if (TierData.perks_data[perk_id].overwrites){
-			if (TierData.perks_data[perk_id].overwrites in perks_original_tier){
-				perk_overwritten_by[TierData.perks_data[perk_id].overwrites]=perk_id;
-			}
+	let perks_list_dom=tier_dom.querySelector(".register-tier-perks-list");
+	for (const inherited_tid of tier_inheritance_chain){
+		let inherited_tdata = TierData.tiers_data[inherited_tid];
+		
+		if ((tier_inheritance_chain.length>1) && (inherited_tid === tier_id)){
+			// This is the original tier, not inherited.
+			let perk_inherit_divider = document.createElement("div");
+			perk_inherit_divider.classList.add(inherited_tdata.css_class);
+			perk_inherit_divider.classList.add("register-tier-perks-inherit-divider");
+			perk_inherit_divider.innerHTML="+";
+			perks_list_dom.appendChild(perk_inherit_divider);
 		}
+		
+		let perks_row_dom=document.createElement("div");
+		perks_row_dom.classList.add("register-tier-perks-row");
+		
+		for( const perk_id of inherited_tdata.perks_list ){
+			let perk_dom = tier_perk_template.content.cloneNode(true);
+			
+			perk_dom.querySelector(".perk-name .langspan-ko").innerHTML=
+				TierData.perks_data[perk_id].name_ko;
+			perk_dom.querySelector(".perk-name .langspan-en").innerHTML=
+				TierData.perks_data[perk_id].name_en;
+			
+			let desc_ko=TierData.perks_data[perk_id].desc_ko;
+			let desc_en=TierData.perks_data[perk_id].desc_en;
+			if (!desc_ko) desc_ko="설명 설명 설명...";
+			if (!desc_en) desc_en="Explanation goes here...";
+			
+			let overwritten_by=perk_overwrite_map[perk_id];
+			if (overwritten_by){
+				perk_dom.querySelector(".register-tier-perk").classList.add("perk-overwritten");
+				desc_ko="아래 <strong>"+TierData.perks_data[overwritten_by].name_ko+"</strong>으로 대체됩니다."
+				desc_en="Replaced by <strong>"+TierData.perks_data[overwritten_by].name_en+"</strong> below."
+			}
+			
+			perk_dom.querySelector(".perk-detail .langspan-ko").innerHTML=
+				desc_ko;
+			perk_dom.querySelector(".perk-detail .langspan-en").innerHTML=
+				desc_en;
+			
+			
+			if (inherited_tid !== tier_id){
+				perk_dom.querySelector(".register-tier-perk").classList.add("perk-inherited");
+			}
+			
+			perk_dom.querySelector(".register-tier-perk").classList.add(
+				inherited_tdata.css_class);
+			
+			perks_row_dom.appendChild(perk_dom);
+		}
+		perks_list_dom.appendChild(perks_row_dom);
 	}
 	
-	let perks_list_dom=tier_dom.querySelector(".register-tier-perks-list");
-	for( const perk_id of perks_id_list ){
-		let perk_dom = tier_perk_template.content.cloneNode(true);
-		
-		perk_dom.querySelector(".perk-name .langspan-ko").innerHTML=
-			TierData.perks_data[perk_id].name_ko;
-		perk_dom.querySelector(".perk-name .langspan-en").innerHTML=
-			TierData.perks_data[perk_id].name_en;
-		
-		let desc_ko=TierData.perks_data[perk_id].desc_ko;
-		let desc_en=TierData.perks_data[perk_id].desc_en;
-		if (!desc_ko) desc_ko="설명 설명 설명...";
-		if (!desc_en) desc_en="Explanation goes here...";
-		
-		if (perk_id in perk_overwritten_by){
-			perk_dom.querySelector(".register-tier-perk").classList.add("perk-overwritten");
-			desc_ko="아래 <strong>"+TierData.perks_data[perk_overwritten_by[perk_id]].name_ko+"</strong>으로 대체됩니다."
-			desc_en="Replaced by <strong>"+TierData.perks_data[perk_overwritten_by[perk_id]].name_en+"</strong> below."
-		}
-		
-		perk_dom.querySelector(".perk-detail .langspan-ko").innerHTML=
-			desc_ko;
-		perk_dom.querySelector(".perk-detail .langspan-en").innerHTML=
-			desc_en;
-		
-		
-		if (perks_original_tier[perk_id] !== tier_id){
-			perk_dom.querySelector(".register-tier-perk").classList.add("perk-inherited");
-		}
-		
-		perk_dom.querySelector(".register-tier-perk").classList.add(
-			TierData.tiers_data[perks_original_tier[perk_id]].css_class);
-		
-		perks_list_dom.appendChild(perk_dom);
-	}
 	
 	tier_dom_inner.addEventListener("click",()=>{
 		tier_dom_inner.classList.toggle("tier-detail-mode");
