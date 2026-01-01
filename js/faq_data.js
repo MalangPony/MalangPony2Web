75d708cb15b816f99c974bdfa8f0ed462fbcac97
SheepPony <sheepponymlp@gmail.com>
2026-01-01 22:52:04 +0900

    Update timetable, faq, participation pages, etc

--- js/faq_data.js
+++ js/faq_data.js
@@ -1,37 +1,102 @@
 
 export let questions=[
+	/*
 	{
-		qtext_ko:"행사에 못 가게 되었습니다. 환불이 가능한가요?",
-		qtext_en:"I cannot make it to the event. Can I get a refund?",
-		atext_ko:"안됩니다<br>무조건 오셔야 합니다<br>안그러면 잡으러감",
-		atext_en:"nah",
+		qtext_ko:"",
+		qtext_en:"",
+		atext_ko:"",
+		atext_en:"",
+		categories:[]
+	},
+	*/
+	{
+		qtext_ko:"행사에 못가게 되었어요! 혹시 환불을 할 수 있을까요?",
+		qtext_en:"I can't make it to the event! Can I get a refund?",
+		atext_ko:"티켓 판매 마감일인 6월 30일 전까지 환불이 가능합니다. 환불이 필요한 경우, 공식 계정을 통해 연락해 주시면 환불을 도와드리겠습니다.",
+		atext_en:"The tickets may be refunded until June 30th, Korean Time. Contact us through any of the official channels to get your ticket refunded.",
 		categories:[]
 	},
 	{
-		qtext_ko:"늦게 입장해도 되나요?",
-		qtext_en:"Is it okay to arrive late?",
-		atext_ko:"늦으면 1초에 백원",
-		atext_en:"you will be fined $1 per 10 seconds",
+		qtext_ko:"양도 가능한가요?",
+		qtext_en:"Can the tickets be transferred?",
+		atext_ko:"티켓 판매 마감일인 6월 30일 전까지 위치폼의 양도 기능을 사용하여 티켓을 자유롭게 양도하실 수 있습니다.",
+		atext_en:"You may use the WitchForm's transfer function to transfer tickets before June 30th. However, if you have purchased the ticket through Ko-Fi, you will need to contact us.",
 		categories:[]
 	},
 	{
-		qtext_ko:"식사시간이 있나요?",
-		qtext_en:"Is there time to grab lunch and dinner?",
-		atext_ko:"말고기 제공됨",
-		atext_en:"raw horse meat will be provided",
+		qtext_ko:"굿즈들 가져가도 되나요?",
+		qtext_en:"Can I show off my merch?",
+		atext_ko:"네, 굿즈를 자유롭게 가져오실 수 있습니다.\"굿즈 전시대\" 코너도 준비되어 있으니, 자신의 굿즈들을 가져와서 자랑해 보세요!",
+		atext_en:"You are free to bring all your pony merch. There's also a dedicated area for showing off your pony merchandise, so brag away!",
 		categories:[]
 	},
 	{
-		qtext_ko:"패널에 어떻게 지원하나요?",
-		qtext_en:"How to apply for a panelist?",
-		atext_ko:'다음 페이지 참고: <span class="link-button inline internal-page-autolink autofill-pagename color-internal generate-icon icon-internal" data-pageid="involved"></span>',
-		atext_en:'Refer to: <span class="link-button inline internal-page-autolink autofill-pagename color-internal generate-icon icon-internal" data-pageid="involved"></span>',
+		qtext_ko:"점심이나 저녁시간은 따로 없나요?",
+		qtext_en:"Is there time for having lunch or dinner?",
+		atext_ko:"점심이나 저녁 시간은 따로 마련되어 있지 않지만, 행사 도중 어느때라도 자유롭게 나가셔서 식사를 하고 오셔도 됩니다.",
+		atext_en:"There's no official lunch or dinner time in the event timetable, but you may go outside and get food any time during the event and come back.",
+		categories:[]
+	},
+	{
+		qtext_ko:"입장은 언제까지 해야하나요?",
+		qtext_en:"Do I need to arrive on-time for the event?",
+		atext_ko:"행사 시작 시간에 맞춰서 오지 않으셔도 무관합니다. 단, 참가증 수령은 18시까지만 가능합니다.",
+		atext_en:"You do not need to arrive on time for the opening ceremonies. However, the registration desk closes at 6PM.",
+		categories:[]
+	},
+	{
+		qtext_ko:"패널 도중에 나가도 되나요?",
+		qtext_en:"Can I leave during a panel?",
+		atext_ko:"패널 도중 자유롭게 입장, 퇴장하셔도 됩니다.",
+		atext_en:"You are free to leave and enter any time during any panel.",
+		categories:[]
+	},
+	{
+		qtext_ko:"저녁시간까지 꼭 남아있어야 하나요?",
+		qtext_en:"Do I need to stay until the end of the event?",
+		atext_ko:"행사 중 어느때나 나가셔도 됩니다.",
+		atext_en:"You can leave any time during the event.",
+		categories:[]
+	},
+	{
+		qtext_ko:"패널, 아트갤러리, 부스를 지원하고 싶어요!",
+		qtext_en:"I want to apply to be a panelist / to submit my art to the art gallery / to be a vendor!",
+		atext_ko:"웹페이지의 \"참가하기\" 페이지들을 확인해 주세요.",
+		atext_en:"Please refer to the \"Participate\" pages on the website",
+		categories:[]
+	},
+	{
+		qtext_ko:"부스에 인원 수 제한이 있나요?",
+		qtext_en:"Is there a limit to how many people can be on a single vendor booth?",
+		atext_ko:"부스인원에 대한 제약은 없습니다! 다만 부스 하나당 의자는 두개씩 주어지며, 부스 운영자 모두 말랑포니 입장권을 따로 구매하셔야 합니다.",
+		atext_en:"There is no hard limit on the number of people. However, there are only two seats per table, and everyone needs to have a badge to the event.",
+		categories:[]
+	},
+	{
+		qtext_ko:"코스프레나 퍼슈팅이 가능한가요?",
+		qtext_en:"Is cosplay and fursuiting allowed?",
+		atext_ko:"코스프레나 퍼슈팅을 하는 것은 자유지만, 행사장에 탈의실이나 지원 시설은 없으니 참고하시기 바랍니다.",
+		atext_en:"You are free to cosplay and fursuit in the venue, but be aware that there are no changing rooms or other supporting facilities in the venue.",
+		categories:[]
+	},
+	{
+		qtext_ko:"행사장 내부에서 음식물 섭취가 가능한가요?",
+		qtext_en:"Can I bring food and eat during the event?",
+		atext_ko:"행사중에 음료나 간단한 간식을 드시는 것은 가능하지만, 냄새나 소리가 다른 참가자들에게 방해가 되지 않도록 해주세요.",
+		atext_en:"You are free to have a snack and drink during the event, but make sure the smell or the noise won't bother the other participants.",
+		categories:[]
+	},
+	{
+		qtext_ko:"질문을 하고 싶어요! 어디서 할 수 있을까요?",
+		qtext_en:"I have other questions! Where can I ask them?",
+		atext_ko:"공식 디스코드, 트위터, 블루스카이, 또는 이메일을 활용해 주세요.",
+		atext_en:"Use our official Discord, Twitter, Bluesky, or email.",
 		categories:[]
 	},
 	{
 		qtext_ko:"최고의 포니는 누구인가요?",
 		qtext_en:"Who is best pony?",
-		atext_ko:"트와일라잇 스파클<br>트와일라잇 스파클<br>트와일라잇 스파클",
+		atext_ko:"트와일라잇 스파클!<br>트와일라잇 스파클!<br>트와일라잇 스파클!",
 		atext_en:"Twilight sparkle!<br>Twilight sparkle!<br>Twilight sparkle!",
 		categories:[]
 	},
